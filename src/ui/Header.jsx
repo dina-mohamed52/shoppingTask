731ec75dff2318f7d5ac95b5a27d40cf832d9839
dina-mohamed52 <dm3969268@gmail.com>
2024-10-31 17:59:18 -0700

    ....updating

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -9,12 +9,12 @@ function Header() {
     
     return (
         <header className="bg-gradient-to-r from-purple-600 via-gray-600 to-pink-600 shadow-md text-white">
-            <div className="container mx-auto flex justify-between items-center p-4">
-                <Link to="/" className="flex items-center">
+            <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center p-4">
+                <Link to="/" className="flex items-center mb-2 sm:mb-0">
                     <RiShoppingBag3Fill fill='#fff' size={30} />
                     <span className="text-3xl font-bold ml-2 font-fantasy">BabyStyle</span>
                 </Link>
-                <nav className="space-x-6 flex items-center mr-[2rem]">
+                <nav className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-6 mr-[2rem]">
                     <Link to="/" className="hover:text-gray-200 transition duration-200">
                         Products
                     </Link>
