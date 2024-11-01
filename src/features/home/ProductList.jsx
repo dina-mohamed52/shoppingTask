7c2a661fb72e45cdb3d548c330dea249aa5d09ac
dina-mohamed52 <dm3969268@gmail.com>
2024-10-31 18:28:39 -0700

    ....updating

--- src/features/home/ProductList.jsx
+++ src/features/home/ProductList.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+
 import { BiCartAdd } from "react-icons/bi";
 import useProducts from "./useProductList";
 import { Tooltip } from 'react-tooltip'; 
-import { Link } from "react-router-dom";
+
 import { useDispatch } from "react-redux";
 import { addToCart } from "../cart/cartSlice";
 import LoadingSpinner from "../../ui/LoadingSpinner";
+import { toast } from "react-toastify";
+
 
 function ProductList() {
     const { data: products, isError, isLoading } = useProducts();
@@ -13,20 +15,25 @@ function ProductList() {
 
     const handleAddToCart = (product) => {
         dispatch(addToCart(product));
+        toast.success("تمت إضافة العنصر إلى السلة بنجاح!", {
+            position: "top-right", 
+            autoClose: 3000, // Duration before the toast disappears
+        });
     };
+    
 
     // Loading and error states
     if (isLoading) return <h1 className="text-center"><LoadingSpinner /></h1>;
     if (isError) return <h1 className="text-center text-red-500">Error</h1>;
 
     return (
-        <div className="container  mx-auto px-6 py-8 w-[95%] rounded-lg mt-12 mb-4 shadow-xl">
+        <div className="container mx-auto px-6 py-8 w-[95%] rounded-lg mt-12 mb-4 shadow-xl">
             {/* Product Grid */}
-            <div className="grid grid-cols-1   sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {products.map((product) => (
                     <div key={product.id} className="bg-white border border-purple-300 rounded-lg shadow-md overflow-hidden transition-transform transform hover:scale-105 hover:shadow-xl">
                         {/* <Link to={`/product/${product.id}`}> */}
-                            <img src={product.img} alt={product.title} className="w-full mt-4 h-56 object-contain" />
+                        <img src={product.img} alt={product.title} className="w-full mt-4 h-56 object-contain" />
                         {/* </Link> */}
                         <div className="p-4">
                             <h2 className="text-lg font-semibold text-gray-800 truncate">{product.title}</h2>
