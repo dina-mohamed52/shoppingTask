c25652e775fb63af83e2020d31bd4b0c45ded76a
dina-mohamed52 <dm3969268@gmail.com>
2025-09-05 16:24:16 -0700

    2st commit

--- src/ui/Header1st.jsx
+++ src/ui/Header1st.jsx
@@ -4,8 +4,8 @@ function Header1st() {
   const { t } = useTranslation();
 
   return (
-    <div className="w-full mx-auto text-center text-gray-800 p-10 sm:p-8 rounded-full shadow-lg border border-gray-300">
-      <h1 className="sm:text-4xl text-xl font-extrabold text-yellow-500 mb-4 tracking-wide">
+    <div className="w-full mx-auto text-center text-gray-800 p-8 sm:p-8 rounded-full shadow-lg border border-gray-300">
+      <h1 className="sm:text-4xl text-lg font-extrabold text-yellow-500 mb-4 tracking-wide">
         {t("header.title")}
       </h1>
 
